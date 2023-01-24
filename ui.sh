64f73101a99e8c60d2b582f09b2ddfef7cf9d54a
Simon Brown <simon@structurizr.com>
2023-01-24 15:38:19 +0000

    Adds a script to copy the UI (which is hosted in a separate repo) into this directory.

---
+++ ui.sh
@@ -0,0 +1,35 @@
+export STRUCTURIZR_UI_DIR=../structurizr-ui
+export STRUCTURIZR_LITE_DIR=.
+
+# JavaScript
+mkdir $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/js
+cp $STRUCTURIZR_UI_DIR/src/js/* $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/js
+
+# CSS
+mkdir $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/css
+cp $STRUCTURIZR_UI_DIR/src/css/* $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/css
+
+# CSS fonts
+mkdir $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/css/fonts
+cp $STRUCTURIZR_UI_DIR/src/css/fonts/* $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/css/fonts
+
+# Images
+mkdir $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/img
+cp $STRUCTURIZR_UI_DIR/src/img/* $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/img
+
+# Bootstrap icons
+mkdir $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/bootstrap-icons
+cp $STRUCTURIZR_UI_DIR/src/bootstrap-icons/* $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/bootstrap-icons
+
+# HTML (offline exports)
+mkdir $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/html
+cp $STRUCTURIZR_UI_DIR/src/html/* $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/html
+
+# JSP fragments
+cp $STRUCTURIZR_UI_DIR/src/fragments/* $STRUCTURIZR_LITE_DIR/src/main/webapp/WEB-INF/fragments
+mkdir $STRUCTURIZR_LITE_DIR/src/main/webapp/WEB-INF/fragments/workspace
+cp $STRUCTURIZR_UI_DIR/src/fragments/workspace/* $STRUCTURIZR_LITE_DIR/src/main/webapp/WEB-INF/fragments/workspace
+
+# JSP
+cp $STRUCTURIZR_UI_DIR/src/jsp/* $STRUCTURIZR_LITE_DIR/src/main/webapp/WEB-INF/jsp
+
