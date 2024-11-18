fc9b17db93ea8746ad160ef6e1c514a2fd748420
Simon Brown <simon@structurizr.com>
2024-11-18 12:30:19 +0000

    Similar change to on-premises script.

--- ui.sh
+++ ui.sh
@@ -1,18 +1,18 @@
+#!/bin/zsh
+
 # - this script merges the contents of the structurizr/ui repository into this directory,
 # - this has only been tested on MacOS
 
 export STRUCTURIZR_BUILD_NUMBER=$1
-
 export STRUCTURIZR_UI_DIR=../structurizr-ui
 export STRUCTURIZR_LITE_DIR=.
 
-mkdir $STRUCTURIZR_LITE_DIR/src/main/resources/static/
-mkdir $STRUCTURIZR_LITE_DIR/src/main/resources/static/static
+rm -rf $STRUCTURIZR_LITE_DIR/src/main/resources/static/static
+mkdir -p $STRUCTURIZR_LITE_DIR/src/main/resources/static/static
 
 # JavaScript
-mkdir $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/js
-rm $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/js/structurizr*.js
-cp $STRUCTURIZR_UI_DIR/src/js/* $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/js
+mkdir -p $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/js
+cp -a $STRUCTURIZR_UI_DIR/src/js/* $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/js
 
 if [[ $STRUCTURIZR_BUILD_NUMBER != "" ]]
 then
@@ -30,38 +30,30 @@ then
 fi
 
 # CSS
-mkdir $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/css
-cp $STRUCTURIZR_UI_DIR/src/css/* $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/css
-
-# CSS fonts
-mkdir $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/css/fonts
-cp $STRUCTURIZR_UI_DIR/src/css/fonts/* $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/css/fonts
+mkdir -p $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/css
+cp -a $STRUCTURIZR_UI_DIR/src/css/* $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/css
 
 # Images
-mkdir $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/img
-cp $STRUCTURIZR_UI_DIR/src/img/* $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/img
+mkdir -p $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/img
+cp -a $STRUCTURIZR_UI_DIR/src/img/* $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/img
 
 # Bootstrap icons
-mkdir $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/bootstrap-icons
-cp $STRUCTURIZR_UI_DIR/src/bootstrap-icons/* $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/bootstrap-icons
+mkdir -p $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/bootstrap-icons
+cp -a $STRUCTURIZR_UI_DIR/src/bootstrap-icons/* $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/bootstrap-icons
 
 # HTML (offline exports)
-mkdir $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/html
+mkdir -p $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/html
 cp $STRUCTURIZR_UI_DIR/src/html/* $STRUCTURIZR_LITE_DIR/src/main/resources/static/static/html
 
 # JSP fragments
-cp $STRUCTURIZR_UI_DIR/src/fragments/* $STRUCTURIZR_LITE_DIR/src/main/webapp/WEB-INF/fragments
-cp $STRUCTURIZR_UI_DIR/src/fragments/workspace/* $STRUCTURIZR_LITE_DIR/src/main/webapp/WEB-INF/fragments/workspace
-mkdir $STRUCTURIZR_LITE_DIR/src/main/webapp/WEB-INF/fragments/diagrams
-cp $STRUCTURIZR_UI_DIR/src/fragments/diagrams/* $STRUCTURIZR_LITE_DIR/src/main/webapp/WEB-INF/fragments/diagrams
-mkdir $STRUCTURIZR_LITE_DIR/src/main/webapp/WEB-INF/fragments/decisions
-cp $STRUCTURIZR_UI_DIR/src/fragments/decisions/* $STRUCTURIZR_LITE_DIR/src/main/webapp/WEB-INF/fragments/decisions
+cp -a $STRUCTURIZR_UI_DIR/src/fragments/* $STRUCTURIZR_LITE_DIR/src/main/webapp/WEB-INF/fragments
+rm -rf $STRUCTURIZR_LITE_DIR/src/main/webapp/WEB-INF/fragments/dsl
 
 # JSP
-cp $STRUCTURIZR_UI_DIR/src/jsp/* $STRUCTURIZR_LITE_DIR/src/main/webapp/WEB-INF/jsp
+cp -a $STRUCTURIZR_UI_DIR/src/jsp/* $STRUCTURIZR_LITE_DIR/src/main/webapp/WEB-INF/jsp
 rm $STRUCTURIZR_LITE_DIR/src/main/webapp/WEB-INF/jsp/review.jsp
 rm $STRUCTURIZR_LITE_DIR/src/main/webapp/WEB-INF/jsp/review-create.jsp
 
 # Java
-mkdir $STRUCTURIZR_LITE_DIR/src/main/java/com/structurizr/util/
+mkdir -p $STRUCTURIZR_LITE_DIR/src/main/java/com/structurizr/util/
 cp $STRUCTURIZR_UI_DIR/src/java/com/structurizr/util/DslTemplate.java $STRUCTURIZR_LITE_DIR/src/main/java/com/structurizr/util/
